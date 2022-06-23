ca3693e3bd0766a6194bb939b19e1d3b98e43a7e
Jon Divi <jonathan.divi@gmail.com>
2022-06-23 19:36:10 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+//===========================
+//Requirements for the server
+const express=require('express');
+const app=express();
+//===========================
+
+
+//===========================
+//CRUD - READ
+app.get(endpoint,callback)
+//===========================
+
+
+
+
+//===========================
+//Listener for server
+app.listen(3000,function(){
+    console.log('Listening on 3000')
+})
+//===========================
+
