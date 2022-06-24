88775ee953c6d8953d723c5ed5f0aa0e64146463
Jon Divi <jonathan.divi@gmail.com>
2022-06-23 20:47:41 -0400

    Adding POST features

--- server.js
+++ server.js
@@ -1,16 +1,39 @@
 //===========================
 //Requirements for the server
 const express=require('express');
+const bodyParser=require('body-parser')
 const app=express();
+
+//===========================
+
+
+//===========================
+//Middleware 
+app.use(bodyParser.urlencoded({extended:true}))
 //===========================
 
 
 //===========================
-//CRUD - READ
-app.get(endpoint,callback)
+//CRUD- Create, Read, Update and Delete
+//ex. app.get(endpoint, callback)
+
+    //===========================
+    //CRUD - READ
+    app.get('/',(req,res)=>{
+        res.sendFile(__dirname+'/index.html')
+    })
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    //===========================
+
+
+    //===========================
+    //CRUD - CREATE
+    app.post('/assets',(req,res)=>{
+        console.log('Helloooooooooo!')
+    })
+    //===========================
+
 //===========================
-
-
 
 
 //===========================
